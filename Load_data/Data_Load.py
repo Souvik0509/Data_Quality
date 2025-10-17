32bdbc78941bfd679c3812640b7a6b36c012c418
souviksarkar1000@gmail.com <souviksarkar1000@gmail.com>
2025-10-18 00:31:29 +0530

    Initital commit for data quality framework

--- Load_data/Data_Load.py
+++ Load_data/Data_Load.py
@@ -1,8 +1,12 @@
-import yaml 
+#import yaml 
 import os
 #from helper.config_load import ConfigYMLLoad
 import pandas as pd 
 import logging 
+from abc import ABC, abstractmethod
+from azure.storage.blob import BlobServiceClient
+from helper.commonutil import CommonUtil
+import io 
 
 # class DataLoader:
 
@@ -11,12 +15,13 @@ import logging
 #         self.config_fle = ConfigYMLLoad.config_load(config_path) 
 
 
-class DataLoader : 
+class DataLoader(ABC) : 
 
     def __init__(self,file_path : str,file_type : str = None):
         self.file_path = file_path
         self.file_type = file_type
 
+    @abstractmethod
     def load_data(self):
         pass 
 
@@ -60,34 +65,41 @@ class ExcelLoader(DataLoader):
             raise e
 
     
+class BlobStorageLoader(DataLoader):
+    def __init__(self,secret_conn_str,container_name,blob_name,blob_path):
+        self.secret_conn_str = secret_conn_str
+        self.container_name = container_name 
+        self.blob_name = blob_name
+        self.blob_path = blob_path
+
+    def load_data(self,keyvault_name: str):
+        try:
+            conn_str = CommonUtil.get_secrets(keyvault_name, self.secret_conn_str)
+            blob_service_client = BlobServiceClient.from_connection_string(conn_str)
+            container_client = blob_service_client.get_container_client(self.container_name)
+            blob_client = container_client.get_blob_client(self.blob_name)
+            download_file_path = os.path.join(self.blob_path, self.blob_name)
+
+            downloader = blob_client.download_blob() 
+            data = downloader.readall()
+            stream = io.BytesIO(data)
+
+            if self.blob_path.lower().endswith('.csv'):
+                df = pd.read_csv(stream)
+            elif self.blob_path.lower().endswith(('.xls', '.xlsx')):
+                df = pd.read_excel(stream)
+            elif self.blob_path.lower().endswith((".parquet", ".pq")):
+                df = pd.read_json(stream)
+            else:
+                raise ValueError(f"Unsupported file format for blob: {self.blob_name}")
+            return df
+
+        except Exception as e:
+            logging.exception(f"Error downloading blob {self.blob_name} from container {self.container_name}")
+            raise e
 
 
-    # def load_data(self):
-    #     try : 
-    #         input_file_path = self.config_fle["input"].get("input_file",None)
-    #         header = 0 if self.config_fle["input"].get("header",True) else None 
-    #         encoder = self.config_fle["input"].get("encoder","utf-8")
-    #         ext = os.path.splitext(input_file_path)[1].lower()
-
-    #         if ext == ".csv"  :
-    #             df = pd.read_csv(input_file_path,header = header,encoding= encoder)
-    #         elif ext == ".parquet":
-    #             df = pd.read_parquet(input_file_path)
-    #         elif ext == ".json" :
-    #             df = pd.read_json(input_file_path)
-    #         elif ext in [".xlsx",".xls"] : 
-    #             df = pd.read_excel(input_file_path)
-    #         else:
-    #             raise (f"unsupported File Type : {ext}")
-    #         logging.info(f"File loaded successfully for : {input_file_path}")
-
-    #         if df.shape[0]!=0:
-    #             return df
-    #         else:
-    #             logging.error(f" {df} :  Dataframe is Empty ")
-    #     except Exception as e :
-    #         logging.exception(f" Error loading file from  {self.config_fle["input"]["input_file"]}")
-    #         raise e
+    
         
 
 
---
+++ helper/commonutil.py
@@ -0,0 +1,71 @@
+import os
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+from sqlalchemy import create_engine    
+from sqlalchemy.engine import Engine
+import urllib 
+import logging 
+import pandas as pd 
+
+
+
+class CommonUtil:
+
+    @staticmethod
+    def validate_file_extension(file_name, allowed_extensions):
+        _, ext = os.path.splitext(file_name)
+        return ext.lower() in allowed_extensions
+    
+    @staticmethod
+    def get_secrets(keyvalut_name: str, secret_name: str) -> str:
+        try:
+            kvault_url = f"https://{keyvalut_name}.vault.azure.net"
+            cred = DefaultAzureCredential()
+            client = SecretClient(vault_url=kvault_url, credential=cred)
+            secret = client.get_secret(secret_name)
+            return secret.value
+        except Exception as e:
+            logging.exception(f" Error retrieving secret {secret_name} from Key Vault {keyvalut_name}")
+            raise e
+        
+
+    @staticmethod 
+    def create_sql_server_engine(server,database,username,password,driver="ODBC Driver 17 for SQL Server"):
+        """
+        Creates a SQLAlchemy engine for connecting to a SQL Server database.
+         """ 
+        
+        quoted_passwrd = urllib.parse.quote_plus(password)
+        #Format = "mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}"
+        connection_string =  (
+        f'mssql+pyodbc://{username}:{quoted_passwrd}@{server}/'
+        f'{database}?driver={driver}')
+        try :
+            engine = create_engine(connection_string)
+            return engine 
+        except Exception as e:
+             logging.exception(" Error creating SQL Server engine")
+             raise e
+
+    @staticmethod
+    def store_data_in_sqldb(engine, table_name: str, df:pd.DataFrame,schema: str = 'dbo',if_exists: str = "replace"):
+        """ 
+        pushing dataset generated from the data quality framework 
+        """
+        try:
+            logging.info(f"Writing DataFrame to SQL Server table: {schema}.{table_name if schema else table_name}")
+            df.to_sql(
+                name = table_name,
+                con = engine,
+                schema = schema, 
+                if_exists = if_exists,  ## want to overwrite for each entry 
+                index = False,
+                chunksize=2000,
+                method = 'multi' 
+
+            )
+            logging.info(f" Succesfully written to {schema}.{table_name} if {schema} else {table_name}")
+        except Exception as e : 
+            logging.exception(f" failed to write dataframe to Sql Server : {e}")
+            raise e 
+        
